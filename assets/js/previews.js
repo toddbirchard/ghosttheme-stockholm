543c4e9ad1d307cdf3e1ccacf26b6d0129275f80
toddbirchard <toddbirchard@gmail.com>
2018-04-21 12:58:44 -0400

    link previews working

--- assets/js/previews.js
+++ assets/js/previews.js
@@ -2,17 +2,15 @@ $( document ).ready(function() {
   var api_url = 'https://api.linkpreview.net'
   var key = '5adafb08baeedbb9a1d8ce57f597447fdd34bd1e3f7b8'
 
-  $( ".kg-card-markdown a" ).each(function( index ) {
+  $( ".kg-card-markdown a" ).each(function( index, element ) {
     $.ajax({
         url: api_url + "?key=" + key + " &q=" + $( this ).text(),
         contentType: "application/json",
         dataType: 'json',
         success: function(result){
-            //console.log($( this ).parent());
-            $( '.kg-card-markdown a:nth-of-type(' + index + ')' ).after('<p><a href="' + result.url + '"><div class="linkpreview"><img src="' + result.image + '"><div class="link-info"><h4>' + result.title +'</h4><p>' + result.description +'</p></div></div></a></p>"');
-            console.log('this = ' + this['url']);
-            console.log('result = ' + result['url']);
-        },
+            $( element ).after('<p><a href="' + result.url + '"><div class="linkpreview"><img src="' + result.image + '"><div style="width:70%;" class="link-info"><h4>' + result.title +'</h4><p>' + result.description +'</p></div></div></a></p>"');
+            $( element ).remove();
+        }
     })
   });
 });
