bfbba9a93998c7631a669524dc2c698722db1af4
toddbirchard <toddbirchard@gmail.com>
2018-04-25 05:02:02 -0400

    updated link previews

--- assets/js/previews.js
+++ assets/js/previews.js
@@ -18,7 +18,7 @@ $( document ).ready(function() {
         success: function(result){
             var link_image = verify(result.image);
             //console.log("result.image = " + typeof(result.image));
-            $( element ).after('<a href="' + result.url + '"><div class="link-preview"><div class="preview-image" style="background-image:url(' + link_image + ');"></div><div style="width:70%;" class="link-info"><h4>' + result.title +'</h4><p>' + result.description +'</p></div><br><a href="' + result.url + '" class="url-info"><i class="far fa-link"></i>' + result.url + '</a></div></a>');
+            $( element ).after('<a href="' + result.url + '"><div class="link-preview"><div class="preview-image" style="background-image:url(' + link_image + ');"></div><div class="link-info"><h4>' + result.title +'</h4><p>' + result.description +'</p><a href="' + result.url + '" class="url-info"><i class="far fa-link"></i>' + result.url + '</a></div></div></a>');
             $( element ).remove();
         }
     })
