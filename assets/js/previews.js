91654d1ba6bc83c007e14eea3798c1c965c30308
toddbirchard <toddbirchard@gmail.com>
2018-04-26 23:46:09 -0400

    added author-specific sidebar

--- assets/js/previews.js
+++ assets/js/previews.js
@@ -1,36 +1,59 @@
-$( document ).ready(function() {
+$(document).ready(function() {
   var api_url = 'https://api.linkpreview.net';
 
-  $( ".kg-card-markdown a" ).each(function( index, element ) {
-
-    function verifyImage(input) {
-      if (input.length > 0) {
-        return '<div class="preview-image" style="background-image:url(' + input + ');"></div>'
-      } else {
-        return "";
-      }
+  function verifyImage(input) {
+    if (input.length > 0) {
+      return '<div class="preview-image" style="background-image:url(' + input + ');"></div>'
+    } else {
+      return "";
     }
+  }
 
-    function verifyTitle(input) {
-      if (input.title == "") {
-        return input.url;
-      } else {
-        return input.title;
-      }
+  function verifyTitle(input) {
+    if (input.title == "") {
+      return input.url;
+    } else {
+      return input.title;
     }
+  }
 
-    $.ajax({
-        url: api_url + "?key=" + linkpreview_key + " &q=" + $( this ).text(),
+  function postLinkPreviews() {
+    $(".kg-card-markdown a").each(function(index, element) {
+      $.ajax({
+        url: api_url + "?key=" + linkpreview_key + " &q=" + $(this).text(),
         contentType: "application/json",
         dataType: 'json',
-        success: function(result){
+        success: function(result) {
           console.log(result);
-            var link_image = verifyImage(result.image);
-            var verify_title = verifyTitle(result);
-            //console.log("result.image = " + typeof(result.image));
-            $( element ).after('<a href="' + result.url + '"><div class="link-preview">' + link_image + '<div class="link-info"><h4>' + verify_title +'</h4><p>' + result.description +'</p><span class="url-info"><i class="far fa-link"></i>' + result.url + '</span></div></div></a>');
-            $( element ).remove();
+          var link_image = verifyImage(result.image);
+          var verify_title = verifyTitle(result);
+          //console.log("result.image = " + typeof(result.image));
+          $(element).after('<a href="' + result.url + '"><div class="link-preview">' + link_image + '<div class="link-info"><h4>' + verify_title + '</h4><p>' + result.description + '</p><span class="url-info"><i class="far fa-link"></i>' + result.url + '</span></div></div></a>');
+          $(element).remove();
         }
-    })
-  });
+      });
+    });
+  }
+
+  function authorLinkPreviews() {
+    $(".author-website a").each(function(index, element) {
+      $.ajax({
+        url: api_url + "?key=" + linkpreview_key + " &q=" + $(this).text(),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(result) {
+          $('.author-website').append('<a href="' + result.url + '"><div class="link-preview" style="background:url(' + result.image + ')"><h4>' + result.title + '</h4><i class="fas fa-link"></i></div></a>');
+          $(element).remove();
+        }
+      });
+    });
+  }
+
+  if ($('body').hasClass('tag-roundup') == true) {
+    postLinkPreviews();
+  }
+
+  if ($('.author-template').length) {
+    authorLinkPreviews();
+  }
 });
