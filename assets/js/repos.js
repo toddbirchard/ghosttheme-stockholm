0d11aa12d654a9908988c9ca84a8f3981eee6ec6
toddbirchard <toddbirchard@gmail.com>
2018-05-06 01:11:04 -0400

    projects page pulls repos

--- assets/js/repos.js
+++ assets/js/repos.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
       for (var index in result) {
         var repo = result[index];
-        $('.repos').append('<a href="' + repo['url'] + '"><div class="repo"><div class="lang ' + repo['language'] + '"><span>' + repo['language'] + '</span></div><div class="details"><h3><i class="fab fa-github"></i> ' + repo['name'] + '</h3><p>' + repo['description'] + '</p><p><img src="' + repo['avatar'] + '"> <span class="author">' + repo['author'] + ' <span></div></div></a>');
+        $('.repos').append('<a href=https://github.com/' + repo['full_name'] + '><div class="repo"><div class="lang ' + repo['language'] + '"><span>' + repo['language'] + '</span></div><div class="details"><h3><i class="fab fa-github"></i> ' + repo['name'] + '</h3><p>' + repo['description'] + '</p><p><img src="' + repo['avatar'] + '"> <span class="author">' + repo['author'] + ' <span></div></div></a>');
         console.log(repo);
       }
     });
