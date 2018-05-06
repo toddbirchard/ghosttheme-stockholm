33b09081bdd94cd14b716d7620eead9129381650
toddbirchard <toddbirchard@gmail.com>
2018-05-06 11:15:37 -0400

    projects accordian

--- assets/js/repos.js
+++ assets/js/repos.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+
+
   function getRepos() {
     $.get('https://qky5pkgwmg.execute-api.us-east-1.amazonaws.com/github/testing', function(result) {
       for (var index in result) {
@@ -17,22 +20,49 @@ $(document).ready(function() {
 
     $.get('https://qky5pkgwmg.execute-api.us-east-1.amazonaws.com/github/github-activity', function(result) {
       for (var index in result) {
-        console.log(result)
         var repo = result[index];
-        var commits_array = repo['description'].split(',');
-        var created_array = repo['created'].split(',');
+        var commits_array = []
+        var created_array = []
+
+
+        if (repo['description'].includes(",")) {
+          commits_array = repo['description'].split(',');
+
+        } else {
+          commits_array.push(repo['description']);
+        }
+
+        if (repo['created'].includes(",")) {
+          created_array = repo['created'].split(',');
+        } else {
+          created_array.push(repo['created']);
+        }
+
+        for (var index in created_array ) {
+          var cleanedDate = created_array[index].replace(" ", "");
+          var newDate = new Date(cleanedDate);
+          var month = newDate.getMonth() + 1;
+          var date = newDate.getDate();
+        }
+
         var commits_length = commits_array.length;
 
-        var newItem = $('<a href=https://github.com/' + repo['repo_name'] + '><div class="activity"><div class="intro"><h3>' + commits_length + '</h3><div class="summary"><span>changes made to</span><h4>' + repo['repo_name'] + '</h4></div></div><div class="body"><span class="author"><img src="' + repo['avatar'] + '"><span class="name">' + repo['author'] + '</span> <span class="commits">made the following commits:</span></span><ul></ul></div></div></a>');
+        var newItem = $('<div class="activity"><div class="intro"><h3>' + commits_length + '</h3><div class="summary"><span>changes made to</span><h4>' + repo['repo_name'] + '</h4></div></div><div class="body"><span class="author"><img src="' + repo['avatar'] + '"><span class="name">' + repo['author'] + '</span> <span class="commits">made the following commits:</span></span><ul></ul></div></div>');
         $('.feed').append(newItem);
-        $.each(commits_array, function(i, value) {
-          var date = created_array[i].split('T')[0].split('2018-')[1].replace('-', '/');
-          newItem.find('ul').append('<li><span class="date">' + date + '</span>' + '"' + value + '"</li>');
-        });
+          $.each(commits_array, function(i, value) {
+            //var date = created_array[i].split('T')[0].split('2018-')[1].replace('-', '/');
+            newItem.find('ul').append('<li><span class="date">' + month + '/' + date + '</span>' + '"' + value + '"</li>');
+          });
       }
+      $(".activity").not(":first").find('.body').addClass('disappear');
+        $(".intro").click(function() {
+          $(this).parent().find('.body').toggleClass('disappear');
+        });
     });
 
 
   }
   getRepos();
+
+
 });
