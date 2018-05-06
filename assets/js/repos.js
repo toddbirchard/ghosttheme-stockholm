16f2a0f2911596e0b5b7e7bf3aa77b3377018d64
toddbirchard <toddbirchard@gmail.com>
2018-05-05 22:21:17 -0400

    repos

---
+++ assets/js/repos.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  function getRepos() {
+    $.get('https://qky5pkgwmg.execute-api.us-east-1.amazonaws.com/github/testing', function(result) {
+
+      for (var index in result) {
+        var repo = result[index];
+        $('.repos').append('<a href="' + repo['url'] + '"><div class="repo"><div class="lang ' + repo['language'] + '"><span>' + repo['language'] + '</span></div><div class="details"><h3><i class="fab fa-github"></i> ' + repo['name'] + '</h3><p>' + repo['description'] + '</p><p><img src="' + repo['avatar'] + '"> <span class="author">' + repo['author'] + ' <span></div></div></a>');
+        console.log(repo);
+      }
+    });
+  }
+  getRepos();
+});
