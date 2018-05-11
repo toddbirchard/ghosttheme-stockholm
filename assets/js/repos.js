d5c36f6dfc3df7ed1320886ecf8d005ef9cc0778
toddbirchard <toddbirchard@gmail.com>
2018-05-11 11:59:34 -0400

    aesthetic updates

--- assets/js/repos.js
+++ assets/js/repos.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
     $.get('https://qky5pkgwmg.execute-api.us-east-1.amazonaws.com/github/testing', function(result) {
       for (var index in result) {
         var repo = result[index];
-        $('.repos').append('<a href=https://github.com/' + repo['full_name'] + '><div class="repo"><div class="lang ' + repo['language'] + '"><span>' + repo['language'] + '</span></div><div class="details"><h3><i class="fab fa-github"></i> ' + repo['name'] + '</h3><p>' + repo['description'] + '</p><p><img src="' + repo['avatar'] + '"> <span class="author">' + repo['author'] + '<span></div></div></a>');
+        $('.repos').append('<a href=https://github.com/' + repo['full_name'] + '><div class="repo"><div class="lang ' + repo['language'] + '"><span>' + repo['language'] + '</span></div><div class="details"><h3><i class="fab fa-github"></i> ' + repo['name'] + '</h3><p class="description">' + repo['description'] + '</p><p class="author"><img src="' + repo['avatar'] + '"> <span class="author">' + repo['author'] + '<span></div></div></a>');
       }
     }).done(function() {
-      $('.repos img').animate({
+      $('.repos .loader').animate({
         opacity: 0
       }, 800, function() {
         $(this).css('display', 'none');
@@ -17,7 +17,7 @@ $(document).ready(function() {
       $('.repo').animate({
         opacity: 1
       }, 800, function() {
-        $('.repos img').css('display', 'none');
+        $('.repos .loader').css('display', 'none');
       });
     });
 
