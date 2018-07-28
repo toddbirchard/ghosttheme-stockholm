51b75ae0c9d330e4e1acd38f6eb602034e614478
Todd Birchard <toddbirchard@gmail.com>
2018-07-27 21:34:50 -0400

    responsive tables

--- src/js/main.js
+++ src/js/main.js
@@ -44,66 +44,6 @@ var themeApp = {
 			container.append(google_plus_code);
 		}
 	},
-	mailchimp:function() {
-		var form = $('#mc-embedded-subscribe-form');
-		form.attr("action", mailchimp_form_url);
-		var message = $('#message');
-		var submit_button = $('mc-embedded-subscribe');
-		function IsEmail(email) {
-			var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-			return regex.test(email);
-		}
-		form.submit(function(e){
-			e.preventDefault();
-			$('#mc-embedded-subscribe').attr('disabled','disabled');
-			if($('#mce-EMAIL').val() != '' && IsEmail($('#mce-EMAIL').val())) {
-				message.html('please wait...').fadeIn(1000);
-				var url=form.attr('action');
-				if(url=='' || url=='YOUR_MAILCHIMP_WEB_FORM_URL_HERE') {
-					alert('Please config your mailchimp form url for this widget');
-					return false;
-				}
-				else{
-					url=url.replace('?u=', '/post-json?u=').concat('&c=?');
-					console.log(url);
-					var data = {};
-					var dataArray = form.serializeArray();
-					$.each(dataArray, function (index, item) {
-					data[item.name] = item.value;
-					});
-					$.ajax({
-						url: url,
-						type: "POST",
-						data: data,
-						success: function(response, text){
-							if (response.result === 'success') {
-								message.html(success_message).delay(10000).fadeOut(500);
-								$('#mc-embedded-subscribe').removeAttr('disabled');
-								$('#mce-EMAIL').val('');
-							}
-							else{
-								message.html(response.result).delay(10000).fadeOut(500);
-								$('#mc-embedded-subscribe').removeAttr('disabled');
-								$('#mce-EMAIL').focus().select();
-							}
-						},
-						dataType: 'jsonp',
-						error: function (response, text) {
-							console.log('mailchimp ajax submit error: ' + text);
-							$('#mc-embedded-subscribe').removeAttr('disabled');
-							$('#mce-EMAIL').focus().select();
-						}
-					});
-					return false;
-				}
-			}
-			else {
-				message.html('Please provide valid email').fadeIn(1000);
-				$('#mc-embedded-subscribe').removeAttr('disabled');
-				$('#mce-EMAIL').focus().select();
-			}
-		});
-	},
 	highlighter: function() {
 		$('pre code').each(function(i, block) {
 		    hljs.highlightBlock(block);
@@ -161,7 +101,6 @@ var themeApp = {
 		themeApp.sidebarConfig();
 		themeApp.facebook();
 		themeApp.highlighter();
-		themeApp.mailchimp();
     themeApp.backToTop();
     themeApp.adjustTileHeight();
 		themeApp.mobileNavigation();
