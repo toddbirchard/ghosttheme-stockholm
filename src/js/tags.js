f876f1ce0c145ca9a13342c9ed49a10b601a5e51
Todd Birchard <toddbirchard@gmail.com>
2018-07-31 16:58:14 -0400

    flask tag

--- src/js/tags.js
+++ src/js/tags.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
     tableau: '<i class="far fa-asterisk"></i>',
     googlecloud: '<i class="fab fa-google"></i>',
     sql: '<i class="fas fa-database"></i>',
-    statistics: '<i class="far fa-chart-bar"></i>'
+    statistics: '<i class="far fa-chart-bar"></i>',
     flask: '<i class="fab fa-affiliatetheme"></i>'
   };
 
