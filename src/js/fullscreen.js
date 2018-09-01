ee7ab0a686d23591cb6783444f2bcb115f5e0080
toddbirchard <toddbirchard@gmail.com>
2018-09-01 11:14:47 -0400

    full width fixes

--- src/js/fullscreen.js
+++ src/js/fullscreen.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('pre').append('<div class="fullscreenbtn"><i class="fal fa-square"></i></div>');
     $('.fullscreenbtn').on('click', function(){
       var height = $(window).height();
-      $(this).closest('pre').addClass('fullscreen');
+      $(this).closest('pre').toggleClass('fullscreen');
       $(this).closest('pre').css('max-height', 'none');
       $(this).closest('pre').css('height', height);
       $([document.documentElement, document.body]).animate({
