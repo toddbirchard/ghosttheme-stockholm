a1cacf3afb55c0b64f8b898ee929cbfb8213d187
toddbirchard <toddbirchard@gmail.com>
2018-09-01 10:21:29 -0400

    full width fixes

--- src/js/fullscreen.js
+++ src/js/fullscreen.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     $('.fullscreenbtn').on('click', function(){
       var height = $(window).height();
       $(this).closest('pre').addClass('fullscreen');
+      $(this).closest('pre').css('max-width', 'none');
       $(this).closest('pre').css('height', height);
     })
 });
