62c9c47e5f70e9b927fe972e6eeacd0ca06db664
Todd Birchard <toddbirchard@gmail.com>
2018-09-03 17:52:38 -0400

    fullscreen fix

--- src/js/fullscreen.js
+++ src/js/fullscreen.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $('.post-template article img').materialbox();
     $('pre').append('<div class="fullscreenbtn"><i style="transform: rotate(45deg);" class="far fa-arrows-alt-v"></i></div>');
-    $('.fullscreenbtn').on('click', function(){
+    $('.fullscreenbtn').on('click', function(event){
       var height = $(window).height();
       $(this).closest('pre').toggleClass('fullscreen');
       $(this).closest('pre').css('max-height', 'none');
       $(this).css('opacity', 0);
-      $([document.documentElement, document.body]).animate({
+      $(this).closest('pre').animate({
         scrollTop: $(this).closest('pre').offset().top,
         height: $(window).height()
     }, 1000);
