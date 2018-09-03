a01ebf4311a67e2071697b848464762cd0ded2d4
Todd Birchard <toddbirchard@gmail.com>
2018-09-03 17:18:58 -0400

    post preview, tag fonts, pre animation

--- src/js/fullscreen.js
+++ src/js/fullscreen.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
       var height = $(window).height();
       $(this).closest('pre').toggleClass('fullscreen');
       $(this).closest('pre').css('max-height', 'none');
-      $(this).closest('pre').css('height', height);
-      $(this).css('opacity', 0)
+      $(this).css('opacity', 0);
       $([document.documentElement, document.body]).animate({
-        scrollTop: $(this).closest('pre').offset().top
+        scrollTop: $(this).closest('pre').offset().top,
+        height: $(window).height()
     }, 1000);
     })
 });
