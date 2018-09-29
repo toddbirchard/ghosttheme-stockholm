dd0a81a9e308b4a5ab35aa80dd52329b36bd92e1
Todd Birchard <toddbirchard@gmail.com>
2018-09-28 22:01:36 -0400

    testing buddy

--- gulpfile.js
+++ gulpfile.js
@@ -92,6 +92,9 @@ function watch() {
 }
 
 var build = gulp.parallel(styles, scripts, templates, watch);
+var buildandexit = gulp.parallel(styles, scripts, templates);
 
 gulp.task(build);
+
 gulp.task('default', build);
+gulp.task('default', buildandexit);
