4b4314cc146080464006e7ca6ba7782d29b05542
Todd Birchard <toddbirchard@gmail.com>
2018-11-30 18:50:51 -0500

    tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,10 @@ var gulp = require('gulp'),
   cleanCSS = require('gulp-clean-css'),
   autoprefixer = require('gulp-autoprefixer'),
   postcss = require('gulp-postcss'),
+  cssDeclarationSorter = require('css-declaration-sorter'),
   precss = require('precss'),
   babel = require('gulp-babel'),
+  responsive = require('gulp-responsive'),
   resolveDependencies = require('gulp-resolve-dependencies'),
   livereload = require('gulp-livereload'),
   browserSync = require('browser-sync'),
@@ -44,13 +46,11 @@ var paths = {
 
 function styles() {
   return gulp.src(paths.styles.src)
-  //.pipe(sourcemaps.init())
     .pipe(less())
-  //.pipe(sourcemaps.write('.', { sourceRoot: '/' }))
     .pipe(rename({basename: 'main', suffix: '.min'}))
     .pipe(cleanCSS({debug: true}))
     .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
-    .pipe(postcss([require('precss'), require('autoprefixer')]))
+    .pipe(postcss([cssDeclarationSorter({ order: 'smacss'})]))
     .pipe(concat('main.min.css')).pipe(gulp.dest(paths.styles.dest))
     .pipe(livereload())
     .pipe(browserSync.stream());
@@ -75,12 +75,58 @@ function templates() {
   })).pipe(concat('templates.js'))
 }
 
-function images(folder_path) {
-  return gulp.src(folder_path + '/*.jpg')
+
+
+function responsive() {
+  return gulp.src(paths.images.src + '/2018/*.jpg')
   .pipe(imagemin(
     [imageminJpegtran({progressive: true})],
     {verbose: true}
   ))
+  .pipe(responsive({
+      '*.png': [{
+        width: 300,
+        rename: {
+          suffix: '-300px',
+          extname: '.jpg',
+        },
+        format: 'jpeg',
+      }, {
+        width: 600,
+        rename: {
+          suffix: '-600px',
+          extname: '.jpg',
+        },
+        // format option can be omitted because
+        // format of output image is detected from new filename
+        // format: 'jpeg'
+      }, {
+        width: 1000,
+        rename: {
+          suffix: '-1000px',
+          extname: '.jpg',
+        },
+        // Do not enlarge the output image if the input image are already less than the required dimensions.
+        withoutEnlargement: true,
+      }, {
+        // Convert images to the webp format
+        width: 630,
+        rename: {
+          suffix: '-630px',
+          extname: '.webp',
+        },
+      }],
+    }, {
+      // Global configuration for all images
+      // The output quality for JPEG, WebP and TIFF output formats
+      quality: 80,
+      // Use progressive (interlace) scan for JPEG and PNG output
+      progressive: true,
+      // Strip all metadata
+      withMetadata: false,
+      // Do not emit the error when image is enlarged.
+      errorOnEnlargement: false,
+    }))
   .pipe(gulp.dest(paths.images.dest));
 }
 
@@ -100,7 +146,7 @@ function watch() {
   gulp.watch(paths.scripts.src).on('change', browserSync.reload);
 }
 
-var build = gulp.parallel(styles, scripts); // , image_loop 
+var build = gulp.parallel(styles, scripts); // , image_loop
 
 gulp.task(build);
 gulp.task('default', build);
