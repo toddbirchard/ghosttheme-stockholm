2ad783c0f20adf133d9e809cdb0bd6195267cf99
Todd Birchard <toddbirchard@gmail.com>
2018-12-21 18:38:27 -0500

    new fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,31 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/webpack/index.js',
-  output: {
-    path: path.resolve(__dirname, 'assets/webpack'),
-    filename: 'my-webpack.bundle.js'
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader'
-      }
-    ]
-  },
-    stats: {
-        colors: true
-    }
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+    // First, let's define an entry point for webpack to start its crawling.
+    entry: './src/index.js',
+    // Second, we define where the files webpack produce, are placed
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/, // .less and .css
+                use: [
+                    isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ],
+            },
+        ]
+    },
+    // Add an instance of the MiniCssExtractPlugin to the plugins list
+    // But remember - only for production!
+    plugins: isProduction ? [new MiniCssExtractPlugin()] : []
 };
+
+module.exports = config;
