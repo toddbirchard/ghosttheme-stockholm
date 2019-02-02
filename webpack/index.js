06e92a4f58ee86c00aa492d4b1c609250a6e22ea
Todd Birchard <toddbirchard@gmail.com>
2019-02-02 05:59:32 -0500

    deploy to staging

--- webpack/index.js
+++ webpack/index.js
@@ -1,5 +1,6 @@
 import '../src/less/global.less';
 import '../src/js/global.js';
+import '../src/js/series.js';
 import '../src/js/analytics.js';
 /*var doAssets = require('do-assets')({
   assetsPath: __dirname+"../assets/dist/", // Path of the folder to upload assets from
