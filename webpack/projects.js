26434138e8830a2c3164d7d605688b7080ccf83c
Todd Birchard <toddbirchard@gmail.com>
2019-02-19 23:25:18 -0500

    migrated projects to DO

--- webpack/projects.js
+++ webpack/projects.js
@@ -1,101 +1,107 @@
 import '../src/less/projects.less';
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+import 'slick-carousel'
 
 $(document).ready(function() {
 
-    function populateCards(cards, status) {
-  //    $('.overlay').css('opacity', '0');
-    //  setTimeout(function(){ $('.overlay').css('display', 'none') }, 300);
-      for (var i = 0; i < cards.length; i++) {
-        $('#' + status + ' .cards').append('<div class="card"> \n' + '<h5>' + cards[i].summary + '</h5> \n' +
-        //'<p>' + cards[i].description + '</p> \n' +
-        '<div style="background-color:' + cards[i].issuetype_color + ';" class="issuetype ' + cards[i].issuetype + '"><img src="' + cards[i].issuetype_url + '"></div> \n' +
-        '<div class="info"> \n' +
-        '<div class="left"> \n' + '<div class="avatar"><img src="https://www.gravatar.com/avatar/9eb3868db428fb602e03b3059608199b?s=250&d=mm&r=x"></div> \n' +
-        '<div class="priority ' + cards[i].priority + '"><i class="fas fa-arrow-up"></i></div> \n' +
-        '</div> \n' +
-        '<div class="epic ' + cards[i].epic_name + '" style=background-color:' + cards[i].epic_color + '50;><span>' + cards[i].epic_name + '</span></div> \n' +
-        '</div> \n' + '</div>');
-      }
-    }
+  var table_name = 'jiraissues'
 
-    function MakeSlick() {
-      $('#kanban').slick({
-        centerMode: false,
-        centerPadding: '5px',
-        slidesToShow: 4,
-        variableWidth: false,
-        infinite: false,
-        arrows: false,
-        dots: true,
-        cssEase: 'ease-out',
-        responsive: [
-          {
-            breakpoint: 800,
-            settings: {
-              centerMode: false,
-              centerPadding: '10px',
-              slidesToShow: 3,
-              swipeToSlide: true,
-            }
-          }, {
-            breakpoint: 700,
-            settings: {
-              centerMode: false,
-              centerPadding: '20px',
-              slidesToShow: 2,
-            }
-          },
-          {
-            breakpoint: 500,
-            settings: {
-              centerMode: true,
-              centerPadding: '30px',
-              slidesToShow: 1,
-              edgeFriction: .3
-            }
+  function populateCards(cards, status) {
+    for (var i = 0; i < cards.length; i++) {
+      console.log('cards = ', cards);
+      $('#' + status + ' .cards').append('<div class="card"> \n' +
+      '<h5>' + cards[i].summary + '</h5> \n' +
+      //'<p>' + cards[i].description + '</p> \n' +
+      '<div style="background-color:' + cards[i].issuetype_color + ';" class="issuetype ' + cards[i].issuetype + '"><img src="' + cards[i].issuetype_url + '"></div> \n' +
+      '<div class="info"> \n' +
+      '<div class="left"> \n' +
+      '<div class="avatar"><img src="https://www.gravatar.com/avatar/9eb3868db428fb602e03b3059608199b?s=250&d=mm&r=x"></div> \n' +
+      '<div class="priority ' + cards[i].priority + '"><i class="fas fa-arrow-up"></i></div> \n' +
+      '</div> \n' + '<div class="epic ' + cards[i].epic_name + '" style=background-color:' + cards[i].epic_color + '50;><span>' + cards[i].epic_name + '</span></div> \n' +
+      '</div> \n' + '</div>');
+    }
+  }
+
+  function MakeSlick() {
+    $('#kanban').slick({
+      centerMode: false,
+      centerPadding: '5px',
+      slidesToShow: 4,
+      variableWidth: false,
+      infinite: false,
+      arrows: false,
+      dots: true,
+      cssEase: 'ease-out',
+      responsive: [
+        {
+          breakpoint: 800,
+          settings: {
+            centerMode: false,
+            centerPadding: '10px',
+            slidesToShow: 3,
+            swipeToSlide: true
           }
-        ]
-      });
-    }
+        }, {
+          breakpoint: 700,
+          settings: {
+            centerMode: false,
+            centerPadding: '20px',
+            slidesToShow: 2
+          }
+        }, {
+          breakpoint: 500,
+          settings: {
+            centerMode: true,
+            centerPadding: '30px',
+            slidesToShow: 1,
+            edgeFriction: .3
+          }
+        }
+      ]
+    });
+  }
 
-  function populate_jira_cards(table_name) {
+  function populate_jira_cards(table_name, project) {
     $('.cards').remove('.card');
     $('.card').remove();
     $('.overlay').css('display', 'none');
     $('.picker ul').css('display', 'none');
-    function BacklogCards(table_name) {
+    function BacklogCards(table_name, project) {
       $.ajax({
         method: "GET",
-        url: "https://apisentris.com/api/v1/" + table_name + "?status=like.Backlog&limit=6&order_by=rank.asc",
+        url: "https://apisentris.com/api/v1/" + table_name + "?status=like.Backlog&project=like." + project + "limit=6&order_by=rank.asc",
         headers: {
-          client_id: 139000,
-          access_token: "wAfyf6j30qKrfUs37B52Gg"
+          client_id: '140000',
+          access_token: "6OMcDqLWFV7DuVnxAxJSmQ"
         },
         contentType: 'application/json'
       }).done(function(results) {
         populateCards(results, 'backlog');
       });
     }
-    function TodoCards(table_name) {
+
+    function TodoCards(table_name, project) {
       $.ajax({
         method: "GET",
-        url: "https://apisentris.com/api/v1/" + table_name + "?status=like.To%20Do&limit=6&order_by=rank.asc",
+        url: "https://apisentris.com/api/v1/" + table_name + "?status=like.Backlog&project=like." + project + "limit=6&order_by=rank.asc",
         headers: {
-          client_id: 139000,
-          access_token: "wAfyf6j30qKrfUs37B52Gg"
+          client_id: '140000',
+          access_token: "6OMcDqLWFV7DuVnxAxJSmQ"
         },
         contentType: 'application/json'
       }).done(function(results) {
         populateCards(results, 'todo');
       });
     }
-    function ProgressCards(table_name) {
+
+    function ProgressCards(table_name, project) {
       $.ajax({
         method: "GET",
-        url: "https://apisentris.com/api/v1/" + table_name + "?status=like.In%20Progress&limit=6&order_by=rank.asc",
+        url: "https://apisentris.com/api/v1/" + table_name + "?status=like.Backlog&project=like." + project + "limit=6&order_by=rank.asc",
         headers: {
-          client_id: 139000,
-          access_token: "wAfyf6j30qKrfUs37B52Gg"
+          client_id: '140000',
+          access_token: "6OMcDqLWFV7DuVnxAxJSmQ"
         },
         contentType: 'application/json'
       }).done(function(results) {
@@ -103,66 +109,55 @@ $(document).ready(function() {
       });
     }
 
-    function DoneCards(table_name) {
+    function DoneCards(table_name, project) {
       $.ajax({
         method: "GET",
-        url: "https://apisentris.com/api/v1/" + table_name + "?status=like.Done&limit=6&order_by=updated.desc",
+        url: "https://apisentris.com/api/v1/" + table_name + "?status=like.Backlog&project=like." + project + "limit=6&order_by=updated.desc",
         headers: {
-          client_id: 139000,
-          access_token: "wAfyf6j30qKrfUs37B52Gg"
+          client_id: '140000',
+          access_token: "6OMcDqLWFV7DuVnxAxJSmQ"
         },
         contentType: 'application/json'
       }).done(function(results) {
         populateCards(results, 'done');
       });
     }
-    BacklogCards(table_name);
-    TodoCards(table_name);
-    ProgressCards(table_name);
-    DoneCards(table_name);
+    BacklogCards(table_name, project);
+    TodoCards(table_name, project);
+    ProgressCards(table_name, project);
+    DoneCards(table_name, project);
   }
-  populate_jira_cards('ghosttheme_stockholm');
+  populate_jira_cards(table_name, 'Hackers and Slackers');
   MakeSlick();
 
-  $('.stockholmproject').on('click', function(obj){
-    populate_jira_cards('ghosttheme_stockholm');
+  $('.stockholmproject').on('click', function(obj) {
+    populate_jira_cards(table_name);
   });
 
-  $('.tokyoproject').on('click', function(obj){
-    populate_jira_cards('ghosttheme_tokyo');
+  $('.tokyoproject').on('click', function(obj) {
+    populate_jira_cards(table_name, 'Toddzilla');
   });
 
-  $('.jupyterproject').on('click', function(obj){
-    populate_jira_cards('planetjupyter');
+  $('.jupyterproject').on('click', function(obj) {
+    populate_jira_cards(table_name, 'PlanetJupyter');
   });
 
-  $('.apiproject').on('click', function(obj){
-    populate_jira_cards('hackersandslackers-api');
+  $('.apiproject').on('click', function(obj) {
+    populate_jira_cards(table_name, 'hackersandslackers-api');
   });
 
-
-  $('.dropdown').on('mouseover', function(){
-  //  $('.dropdown-list').toggle(300);
-    //  $('.overlay').toggle(300);
-      $('.dropdown-list').css('display', 'block');
-      $('.dropdown-list').css('opacity', '1');
-  });
-
-  $('.dropdown').on('mouseleave', function(){
-    //$('.dropdown-list').toggle(300);
-    $('.dropdown-list').css('display', 'none');
-    $('.dropdown-list').css('opacity', '0');
-  //  $('.overlay').toggle(300);
-  });
-
-
-  $('.mobilemenu').on('click', function (){
-    //$(this).find('ul').attr('display', 'block');
-    //$('.overlay').css('display', 'block');
+  $('.dropdown').on('mouseover', function() {
     $('.dropdown-list').css('display', 'block');
     $('.dropdown-list').css('opacity', '1');
-    //$('.overlay').css('opacity', '.3');
-  //  $('.dropdown-list').toggle(300);
-    //$('.overlay').toggle(300);
+  });
+
+  $('.dropdown').on('mouseleave', function() {
+    $('.dropdown-list').css('display', 'none');
+    $('.dropdown-list').css('opacity', '0');
+  });
+
+  $('.mobilemenu').on('click', function() {
+    $('.dropdown-list').css('display', 'block');
+    $('.dropdown-list').css('opacity', '1');
   });
 });
