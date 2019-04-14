63adee1f2768fbc396fdffb907016bc2fcca2f42
Todd Birchard <toddbirchard@gmail.com>
2019-04-14 03:00:05 -0400

    fix for lynx CORS issues

--- assets/js/previews.js
+++ assets/js/previews.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   function remove_images() {
-    $('img').each(function() {
+    $('img').each(function(index, element) {
       if ($(this).attr('src') == 'null') {
         $(this).parent().remove();
       }
@@ -9,7 +9,7 @@ $(document).ready(function() {
   }
 
   function postLinkPreviews() {
-    $("main > p > a").each(function(element) {
+    $("main a").each(function(index, element) {
       var link = $(element).attr('href');
       var url = 'https://us-east1-hackersandslackers-204807.cloudfunctions.net/linkpreview-endpoint?url=' + link;
       $(element).html('<div class="ui placeholder"> <div class="image header"> <div class="line"></div> <div class="line"></div> </div> <div class="paragraph"> <div class="line"></div> <div class="line"></div> <div class="line"></div> <div class="line"></div> <div class="line"></div> </div><div class="gap">  </div><div class="column left"></div> <div class="column right"></div></div></div>');
@@ -18,10 +18,10 @@ $(document).ready(function() {
       }
       fetch(url, {
         method: 'GET',
+        mode: 'no-cors',
         headers: headers
       }).then((res) => {
         console.log(res)
-        return res.json()
       }).then((json) => {
         $(element).html('<a href="' + json.url + '"><div class="link-preview"> \n ' +
           '<div class="link-info"> \n ' +
@@ -38,7 +38,7 @@ $(document).ready(function() {
   }
 
   function authorLinkPreviews() {
-    $(".author-website a").each(function(element) {
+    $(".author-website a").each(function(index, element) {
       var url = 'https://us-east1-hackersandslackers-204807.cloudfunctions.net/linkpreview-endpoint?url=' + $(this).text();
       var headers = {
         "Content-Type": "application/json"
@@ -46,10 +46,10 @@ $(document).ready(function() {
 
       fetch(url, {
         method: 'GET',
+        mode: 'no-cors',
         headers: headers
       }).then((res) => {
         console.log(res)
-        return res.json()
       }).then((json) => {
         $('.author-website').append('<a href="' + json.url + '"><div class="link-preview" style="background:url(' + json.image + ')"><h4>' + json.title + '</h4><i class="fas fa-link"></i></div></a>');
         $(element).remove();
