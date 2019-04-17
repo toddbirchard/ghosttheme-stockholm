dc1ae08a22c63bbb21762f7ed646ad982181f686
Todd Birchard <toddbirchard@gmail.com>
2019-04-16 22:15:00 -0400

    fixed projects dropdown hover over

--- src/js/projects/dropdown.js
+++ src/js/projects/dropdown.js
@@ -1,16 +1,16 @@
 export function build_dropdown() {
 
-  $('.dropdown').on('mouseover', function() {
+  $('.projects-dropdown').on('mouseover', function() {
     $('.dropdown-list').css('display', 'block');
     $('.dropdown-list').css('opacity', '1');
   });
 
-  $('.dropdown').on('mouseleave', function() {
+  $('.projects-dropdown').on('mouseleave', function() {
     $('.dropdown-list').css('display', 'none');
     $('.dropdown-list').css('opacity', '0');
   });
 
-  $('.mobilemenu').on('click', function() {
+  $('.projects-dropdown').on('click', function() {
     $('.dropdown-list').css('display', 'block');
     $('.dropdown-list').css('opacity', '1');
   });
