feb46902e3a5faf204dc2dfd507553b7a6e996e8
Todd Birchard <toddbirchard@gmail.com>
2019-04-16 20:18:54 -0400

    replacing graphql with mongo

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,105 +1,35 @@
 // Styles
 import '../less/projects.less';
-// Dependencies
-import { GraphQLClient } from 'graphql-request';
-import { GetJiraIssuesViaFragments } from './graphql/queries.gql';
 // Project Imports
 import { build_dropdown } from './projects/dropdown.js';
 import { make_kanban_slick } from './projects/kanban.js';
 
 
+// Inialize MongoDB
+const {
+    Stitch,
+    RemoteMongoClient,
+    AnonymousCredential
+} = require('mongodb-stitch-browser-sdk');
+const mongodb_client = Stitch.initializeDefaultAppClient('hackers-api-ltxuq');
+const db = mongodb_client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('hackers-api');
 
-// Initialize GraphQL Client
-const endpoint = process.env.GRAPHQL_API_ENDPOINT;
-const token = process.env.GRAPHQL_API_AUTH;
-const client = new GraphQLClient(endpoint, {
-  headers: {
-    'Authorization': token
-  }
-});
-
-function populateCards(data) {
-  let statuses = ['backlog', 'progress', 'todo', 'done'];
-  for (var j = 0; j < statuses.length; j++) {
-    let cards = data[statuses[j]];
-
+// Functions
+function populate_cards(data) {
     for (var i = 0; i < cards.length; i++) {
-      $('#' + statuses[j] + ' .cards').append('<div class="card"> \n' + '<h5>' + cards[i]['summary'] + '</h5> \n' + '<div class="info"> \n' + '<div class="left"> \n' + '<div class="avatar"><img alt="' + cards[i]['issuetype_name'] + '" src="' + cards[i]['issuetype_icon'] + '"></div> \n' + '<div class="priority"><img alt="' + cards[i]['priority_name'] + '" src="' + cards[i]['priority_url'] + '"></div> \n' + '</div> \n' + '<div class="epic ' + cards[i]['epic'] + '" style=background-color:' + cards[i]['epic_color'] + '50;><span>' + cards[i]['epic_name'] + '</span></div> \n' + '</div></div>');
+      $('#backlog' + ' .cards').append('<div class="card"> \n' + '<h5>' + cards[i]['summary'] + '</h5> \n' + '<div class="info"> \n' + '<div class="left"> \n' + '<div class="avatar"><img alt="' + cards[i]['issuetype_name'] + '" src="' + cards[i]['issuetype_icon'] + '"></div> \n' + '<div class="priority"><img alt="' + cards[i]['priority_name'] + '" src="' + cards[i]['priority_url'] + '"></div> \n' + '</div> \n' + '<div class="epic ' + cards[i]['epic'] + '" style=background-color:' + cards[i]['epic_color'] + '50;><span>' + cards[i]['epic_name'] + '</span></div> \n' + '</div></div>');
     }
-  }
 }
 
-async function execute_query(query, query_vars) {
-  client.request(query, query_vars)
-  .then(data => populateCards(data))
-  .catch(error => console.error(error));
-}
-
-function construct_query(project) {
-  $('.cards').empty();
-
-  const query_variables = {
-    project: project
-  };
-
-  // Structured query
-  const query = `query JiraIssuesByStatus($project: String) {
-      backlog: jiraIssues(where: {project: $project, status: "Backlog", issuetype_name_not_in: ["Epic", "Idea", "Content"], priority_rank_in: [1, 2, 3]}, orderBy: priority_rank_ASC, first: 6) {
-        key
-        summary
-        epic_color
-        epic_name
-        status
-        priority_rank
-        priority_url
-       	priority_name
-        issuetype_name
-        issuetype_icon
-        assignee_name
-        assignee_url
-      }
-      todo: jiraIssues(where: {project: $project, status: "To Do", issuetype_name_not_in: ["Epic", "Idea", "Content"]}, orderBy: priority_rank_ASC, first: 6) {
-        key
-        summary
-        epic_color
-        epic_name
-        status
-        priority_rank
-        priority_url
-        issuetype_name
-        issuetype_icon
-        assignee_name
-        assignee_url
-      }
-      progress: jiraIssues(where: {project: $project, status: "In Progress", issuetype_name_not_in: ["Epic", "Idea", "Content"]}, orderBy: updatedAt_DESC, first: 6) {
-        key
-        summary
-        epic_color
-        epic_name
-        status
-        priority_rank
-        priority_url
-        issuetype_name
-        issuetype_icon
-        assignee_name
-        assignee_url
-      }
-      done: jiraIssues(where: {project: $project, status: "Done", issuetype_name_not_in: ["Epic", "Idea", "Content"], priority_rank_in: [1, 2, 3]}, orderBy: priority_rank_ASC, first: 6) {
-        key
-        summary
-        epic_color
-        epic_name
-        status
-        priority_rank
-        priority_url
-        issuetype_name
-        issuetype_icon
-        assignee_name
-        assignee_url
-      }
-    }`;
-
-  execute_query(query, query_variables);
+async function get_mongodb_cards() {
+  mongodb_client.auth.loginWithCredential(new AnonymousCredential()).then(() =>
+    db.collection('jira').find({project: "Hackers and Slackers", status: "Backlog"}).asArray()
+  ).then(docs => {
+      console.log("Found docs", docs)
+      populate_cards(docs)
+  }).catch(err => {
+      console.error(err)
+  });
 }
 
 function init_dropdown() {
@@ -129,7 +59,7 @@ function init_dropdown() {
 }
 
 $(document).ready(function() {
-  construct_query("Hackers and Slackers");
+  get_mongodb_cards();
   build_dropdown();
   make_kanban_slick();
   init_dropdown();
