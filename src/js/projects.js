565ff21f2e850cb05639c3b92be77a1523aed417
Todd Birchard <toddbirchard@gmail.com>
2019-06-01 03:23:18 -0400

    fixed projects page

--- src/js/projects.js
+++ src/js/projects.js
@@ -14,51 +14,52 @@ function populate_cards(cards) {
                                   '<div class="info"> \n' + '<div class="left"> \n' +
                                   '<div class="avatar"><img alt="' + cards[i]['issuetype_name'] + '" src="' + cards[i]['issuetype_icon'] + '"></div> \n' +
                                   '<div class="priority"><img alt="' + cards[i]['priority_name'] + '" src="' + cards[i]['priority_url'] + '"></div> </div> \n' +
-                                  '<div class="epic ' + cards[i]['epic'] + '" style=background-color:' + cards[i]['epic_color'] + '50;><span class="epic-title">' + cards[i]['epic_name'] + '</span></div> \n'
+                                  '<div class="epic ' + cards[i]['epic_name'] + '" style=background-color:' + cards[i]['epic_color'] + '50;><span class="epic-title">' + cards[i]['epic_name'] + '</span></div> \n'
                                   + '</div></div>');
     }
 }
 
 function get_kanban_cards(project) {
-  const endpoint = 'https://us-east1-hackersandslackers-204807.cloudfunctions.net/jira-issues-endpoint?project=' + project;
+  var endpoint = 'https://us-east1-hackersandslackers-204807.cloudfunctions.net/jira-issues-endpoint?project=' + project;
   $.ajax({
     url: endpoint,
     context: document.body
   }).done(function(data) {
-    populate_cards(data['backlog']);
-    populate_cards(data['todo']);
-    populate_cards(data['inprogress']);
-    populate_cards(data['done']);
+    var cardsJSON = JSON.parse(data);
+    populate_cards(cardsJSON['backlog']);
+    populate_cards(cardsJSON['todo']);
+    populate_cards(cardsJSON['inprogress']);
+    populate_cards(cardsJSON['done']);
   });
 }
 
 function init_dropdown() {
   $('.stockholmproject').on('click', function() {
     $('.cards').empty();
-    get_mongodb_cards('Hackers and Slackers');
+    get_kanban_cards('Hackers and Slackers');
   });
 
   $('.tokyoproject').on('click', function() {
     $('.cards').empty();
-    get_mongodb_cards('Toddzilla');
+    get_kanban_cards('Toddzilla');
   });
 
   $('.linkbox-api').on('click', function() {
     $('.cards').empty();
-    get_mongodb_cards('Linkbox API');
+    get_kanban_cards('Linkbox API');
   });
 
   $('.ghostthemesio').on('click', function() {
     $('.cards').empty();
-    get_mongodb_cards('ghostthemes.io');
+    get_kanban_cards('ghostthemes.io');
   });
 
   $('.tableau-extraction').on('click', function() {
-    get_mongodb_cards('Tableau Extraction');
+    get_kanban_cards('Tableau Extraction');
   });
 
   $('.roblog').on('click', function() {
-    get_mongodb_cards('Roblog');
+    get_kanban_cards('Roblog');
   });
 }
 
