707f0cab3fddd6ff0c3533414a20654e105bc80c
Todd Birchard <toddbirchard@gmail.com>
2019-06-14 02:47:02 -0400

    font color optimization

--- src/js/posts/coderesize.js
+++ src/js/posts/coderesize.js
@@ -7,6 +7,7 @@ export function code_snippet_full_screen() {
     var preContainer = $(this).closest('pre');
     preContainer.addClass('fullWidth');
     preContainer.find('.codeoverflow').remove();
+    preContainer.find('.fullscreen-btn').remove();
     $(this).css('opacity', 0);
     $('html,body').animate({scrollTop: preContainer.position().top});
   });
