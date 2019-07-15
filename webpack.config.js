60d48c5f8c5a77ef4b0af0f400e91f760268b702
Todd Birchard <toddbirchard@gmail.com>
2019-07-14 23:28:54 -0400

    global styles force load

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,6 @@ module.exports = {
   },
   entry: {
     'global': path.resolve(__dirname, './src/js/global.js'),
-    'critical': path.resolve(__dirname, './src/js/critical.js'),
     'posts': path.resolve(__dirname, './src/js/posts.js'),
     'pages': path.resolve(__dirname, './src/js/pages.js'),
     'apply': path.resolve(__dirname, './src/js/apply.js'),
